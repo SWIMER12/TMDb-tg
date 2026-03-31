c702a5ed2443cc242d179a2b13fa2dd2e802f985
SWIMER12 <t3452170@gmail.com>
2026-03-31 15:36:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def setup_webhook():
 
 @app.route(f"/{TOKEN}", methods=["POST"])
 def webhook():
-    if request.headers.get('content-type') == 'application/json':
+    if 'application/json' in request.headers.get('content-type', ''):
         json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
